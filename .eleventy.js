e39b8c8e4290e53bed993d4c68559b4fe55ab6b4
Joshua Edfonce <joshaffilly@gmail.com>
2026-02-25 23:58:43 +0300

    still fixing the travel ad image

--- .eleventy.js
+++ .eleventy.js
@@ -445,7 +445,14 @@ module.exports = function (eleventyConfig) {
     return `
 <div class="w-full h-30 mt-40 mb-40">
   <div class="travel-ad-img h-30 h-40-sm">
-    <img src="https://res.cloudinary.com/dxqnpsj8k/image/upload/v1768648523/soneva-jani-overwater-villa_sauyxe.jpg" alt="Travel Destination" />
+    <img 
+       src="https://res.cloudinary.com/dxqnpsj8k/image/upload/v1768648523/soneva-jani-overwater-villa_sauyxe.jpg" 
+       alt="Travel Destination"
+        loading="lazy"
+        width="600"
+        height="400"
+        class="m-0 rounded-none"  
+    />
     <div class="travel-ad-overlay"></div>
     <div class="travel-ad-text">
       <h3 id="travel-ad-heading"></h3>
